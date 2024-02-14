d6adf62bc9896d8ac5240f0043f0baa2e1bcc2ed
abhishek mishra <abhms200@gmail.com>
2024-02-14 23:49:37 +0530

    social media added with experience

--- app/component/Home.tsx
+++ app/component/Home.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin,faSquareXTwitter,faMedium ,faGithub} from '@fortawesome/free-brands-svg-icons';
 const Home = () => {
   return (
     <div className="max-w-screen-xl flex flex-col md:flex-row items-center justify-between mx-auto">
@@ -18,7 +19,15 @@ const Home = () => {
           alt={"Abhishek Mishra"}
         />
       </div>
-
+      <span className="experience">
+        <span className="experience_number">
+          <h1>2</h1>
+        </span>
+        <span className="experience_text">
+          <h4>Almost Years</h4>
+          <h4>Experience</h4>
+        </span>
+      </span>
       <div>
         <h4 className="hey-there name aboutpara">
           I develop beautiful things and I love what I do.
@@ -29,6 +38,15 @@ const Home = () => {
         <h4 className="hey-there name aboutpara">
           {process.env.NEXT_PUBLIC_EMAIL}
         </h4>
+
+        <h3><FontAwesomeIcon icon={faLinkedin} /></h3>
+        <h3><FontAwesomeIcon icon={faSquareXTwitter} /></h3>
+        <h3><FontAwesomeIcon icon={faMedium} /></h3>
+        <h3><FontAwesomeIcon icon={faGithub} /></h3>
+        <img src="/leetcode.svg" alt="leetcode" width={"15px"}/>
+      </div>
+      <div className="cert">
+
       </div>
     </div>
   );
